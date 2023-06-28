3331cc0827e1d190c02b406b608b7f9751697c58
JoshGaona14 <jdgvs142001@gmail.com>
2023-06-28 00:08:22 -0500

    Database initialized

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import handleSubmit from './src/firebase_init/handlesubmit';
+import { useRef } from 'react';
 
 export default function App() {
+  const dataRef = useRef()
+ 
+  const submithandler = (e) => {
+    e.preventDefault()
+    handleSubmit(dataRef.current.value)
+    dataRef.current.value = ""
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <form onSubmit={submithandler}>
+        <input type= "text" ref={dataRef} />
+        <button type = "submit">Save</button>
+      </form>
     </View>
   );
 }
@@ -18,3 +31,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ src/firebase_init/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXBDXZU8yoiykBHaaaIfPr4KIxLFkgc4E",
+  authDomain: "uni-link-b79d0.firebaseapp.com",
+  projectId: "uni-link-b79d0",
+  storageBucket: "uni-link-b79d0.appspot.com",
+  messagingSenderId: "286725607317",
+  appId: "1:286725607317:web:7c9620e65166dfffa7b09c",
+  measurementId: "G-5KPCRH3GPC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
