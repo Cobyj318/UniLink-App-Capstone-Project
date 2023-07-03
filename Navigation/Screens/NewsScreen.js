83dbf8694303774f11b96edeb2fbf31999dd2cbe
Cobyj318 <Cobyj318@gmail.com>
2023-07-03 10:21:43 -0500

    Create NewsScreen.js

---
+++ Navigation/Screens/NewsScreen.js
@@ -0,0 +1,20 @@
+import * as React from 'react' ;
+import {View, Text} from 'react-native';
+
+export default function NewsScreen({navigation}){
+    return(
+        <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
+            <Text
+                onPress={()=> alert('This is the "News" screen')}
+                style={{ fontSize:26, fontWeight:'bold'}}>
+                    News
+            </Text>   
+            <Text
+        onPress={()=> alert('You have RSVP')}
+        style={{ fontSize:26, fontWeight:'bold'}}>
+            Create Status Post
+        </Text>
+        </View> 
+    );
+}
+
