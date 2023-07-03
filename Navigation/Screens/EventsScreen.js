0ea9f428bf4d40ff00ee8327ff236a9433a60afe
Erik-Romero <126374892+Erik-Romero@users.noreply.github.com>
2023-07-03 09:03:20 -0500

    Attempt on Kitten UI

--- Navigation/Screens/EventsScreen.js
+++ Navigation/Screens/EventsScreen.js
@@ -1,21 +1,75 @@
 import * as React from 'react' ;
-import {View, Text,Pressable} from 'react-native';
+import {StyleSheet,View} from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider,Card, Layout, Text, Input,Button, Modal  } from '@ui-kitten/components';
+
 
 export default function EventsScreen({navigation}){
+    
+  const [value, setValue] = React.useState('');
+  const [value1,setValue2] = React.useState('');
+
+  const [visible, setVisible] = React.useState(false);
     return(
+<ApplicationProvider {...eva} theme={eva.light}>
     <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
-        <Text
+            
+            <Text
             onPress={()=> alert('This is the "Events" screen')}
-            style={{ fontSize:26, fontWeight:'bold'}}>
+            style={{ fontSize:50, fontWeight:'bold'}}>
                 Events screen
-        </Text>    
+            </Text>  
+            
     
-        <Text
-        onPress={()=> alert('You have RSVP')}
-        style={{ fontSize:26, fontWeight:'bold'}}>
+        <Button style={{ fontSize:26, fontWeight:'bold'}}>
             RSVP
-        </Text>
-    
+        </Button> 
+           
+        <Button 
+            onPress={() => setVisible(true)} 
+            style={{ fontSize:26, fontWeight:'bold'}}>
+                Create Event
+        </Button> 
+        <Modal
+        visible={visible}
+        backdropStyle={styles.backdrop}
+        onBackdropPress={() => setVisible(false)}
+        > 
+            <Card disabled={true}>
+            <Text 
+                style={{ fontSize:25, fontWeight:'bold'}}>
+                Input event details!
+            </Text>
+
+            <Input
+            placeholder='Name of your event'
+            value={value}
+            onChangeText={nextValue => setValue(nextValue)}
+            />
+             <Input
+            placeholder='Details'
+            value={value1}
+            onChangeText={nextValue => setValue2(nextValue)}
+            />
+            <Button onPress={() => setVisible(false)}>
+                Enter
+            </Button>
+            <Button onPress={() => setVisible(false)}>
+                DISMISS
+            </Button>
+            </Card>
+        </Modal>
     </View>
+</ApplicationProvider>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      minHeight: 192,
+    },
+    backdrop: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+  });
