1b05fb7f281beef7ee976c538c2669efffc5cab6
Erik-Romero <126374892+Erik-Romero@users.noreply.github.com>
2023-07-14 12:50:54 -0500

    Save JSON Files
    Added a function to be able to save JSON files to directory.

--- src/firebase_init/getUserEvents.js
+++ src/firebase_init/getUserEvents.js
@@ -1,12 +1,17 @@
 
 import { collection, getDocs } from "firebase/firestore"; 
 import { firestore } from "./firebase"
+import saveJSONLocally from "./saveJSONLocally";
+import * as FileSystem from 'expo-file-system';
 
 const getUserEvents = async () => 
 {    
-const querySnapshot = await getDocs(collection(firestore, "Event_data"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-});
+  const filePath = FileSystem.documentDirectory + 'posts.json';
+  const querySnapshot = await getDocs(collection(firestore, "Event_data"));
+  querySnapshot.forEach(async (doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    saveJSONLocally(filePath,JSON.stringify(doc.data()));
+  });
 }
+
 export default getUserEvents
